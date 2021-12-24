cdb31c99416dc1fbccb78c286e00bf78950ef058
salwa120 <96533459+salwa120@users.noreply.github.com>
2021-12-24 19:46:45 +0500

    Add files via upload

---
+++ visitor-labtask/visitorpattern/src/visitorpattern/Rectangle.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visitorpattern;
+
+/**
+ *
+ * @author  Calypso Computers
+ */
+public class Rectangle extends Dot implements Shape {
+    
+    private int width;
+    private int height;
+
+    public Rectangle(int id, int x, int y, int width, int height) {
+        super(id, x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitRectangle(this);
+    }
+
+    @Override
+    public void move(int x, int y) {
+        // move shape
+    }
+
+    @Override
+    public void draw() {
+        // draw shape
+    }
+
+   
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
